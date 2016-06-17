f16d7ed9d37750f7193421f239ad37d522aa30d2
mcasciato <michael.casciato@icloud.com>
2016-06-17 17:44:01 -0400

    working on post user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,13 +23,22 @@ class UsersController < ApplicationController
     end
   end
 
-
   get '/login' do
-  if logged_in?
-    redirect to '/instruments'
-  else
-    erb :'/users/login'
+    if logged_in?
+      redirect to '/instruments'
+    else
+      erb :'/users/login'
+    end
   end
-end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/instruments"
+    else
+      erb :'/users/login'
+    end
+ end
 
 end
