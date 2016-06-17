6f2674f576fb8c7fdbfc4ac163559559b2e54138
mcasciato <michael.casciato@icloud.com>
2016-06-17 17:59:16 -0400

    added slugifiable; may remove later if not needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :instruments
   validates_presence_of :username, :email, :password
+
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
 end
