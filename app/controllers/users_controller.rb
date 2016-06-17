c761908c6f0da076caa159755b076899b568294a
mcasciato <michael.casciato@icloud.com>
2016-06-17 15:41:15 -0400

    user controller can view login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,16 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'/users/signup'
     else
-      erb :index
+      erb :'instruments/instruments'
     end
   end
+
+  get '/login' do
+  if logged_in?
+    redirect to '/instruments'
+  else
+    erb :'/users/login'
+  end
+end
 
 end
