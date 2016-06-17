a8e6ddbe8edbbaf8fd6062aadd965376239af362
mcasciato <michael.casciato@icloud.com>
2016-06-17 16:19:52 -0400

    started instrument controller

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,4 +1,13 @@
 class InstrumentsController < ApplicationController
 
+  get '/instruments' do
+    if !logged_in?
+      redirect to '/login'
+      # flash[:message] = "You must be logged in to view instruments."
+    else
+      @instruments = Instrument.all
+      erb :'/instruments/instruments'
+    end
+  end
 
 end
